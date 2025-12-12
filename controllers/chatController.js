5825c9a318b57ef866fa2def0273a6e4f274fe41
Iraj Tahir <irajtahir555@gmail.com>
2025-12-12 23:37:20 +0500

    fixed

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,10 +1,13 @@
 const Message = require("../models/Message");
 const askHF = require("../services/aiService"); // updated HF service
 
-// Fetch all messages of logged-in user
+// Fetch all chat messages of logged-in user (exclude activity insights)
 const getMessages = async (req, res) => {
   try {
-    const messages = await Message.find({ user: req.user._id }).sort({ createdAt: 1 });
+    const messages = await Message.find({
+      user: req.user._id,
+      type: "chat" // <-- only chat messages
+    }).sort({ createdAt: 1 });
     res.json(messages);
   } catch (err) {
     console.error("Fetch Messages Error:", err);
