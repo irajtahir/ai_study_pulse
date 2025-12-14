758062558b56ee47f98fbb791b29af8b7335c90e
Iraj Tahir <irajtahir555@gmail.com>
2025-12-14 19:05:47 +0500

    fixed

---
+++ routes/student.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const { joinClass, getStudentClasses, getClassById } = require("../controllers/teacherController");
+const { protect } = require("../middleware/auth"); // assuming you already have auth middleware
+
+// Join class by code
+router.post("/classes/join", protect, joinClass);
+
+// Get all joined classes
+router.get("/classes", protect, getStudentClasses);
+
+// Get details of a class (assignments, materials, etc.)
+router.get("/classes/:id", protect, getClassById);
+
+module.exports = router;
