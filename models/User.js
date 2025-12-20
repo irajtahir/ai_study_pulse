783280be7f1aba9ad8724af0500e40b060570d06
Iraj Tahir <irajtahir555@gmail.com>
2025-12-20 15:08:27 +0500

    fixed

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['student','teacher','admin'], default: 'student' },
+  classes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Class" }],
   createdAt: { type: Date, default: Date.now }
 });
 
