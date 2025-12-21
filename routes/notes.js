958b030749f3bd3a97f026a0e1c3d7834c939981
Iraj Tahir <irajtahir555@gmail.com>
2025-12-21 09:54:17 +0500

    fixed

--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,10 @@
-// backend/routes/notes.js
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/authMiddleware');
 const notesController = require('../controllers/notesController');
 
 router.get('/', auth, notesController.getNotes);
+router.get('/stats', auth, notesController.getStats); 
 router.get('/:id', auth, notesController.getNoteById);
 router.post('/', auth, notesController.createNote);
 router.put('/:id', auth, notesController.updateNote); 
