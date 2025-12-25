cb78a18e0b1bdacdf41e2709db01aedfd4fb76cc
Iraj Tahir <irajtahir555@gmail.com>
2025-12-25 14:39:28 +0500

    fixed

--- test.js
+++
@@ -1,6 +0,0 @@
-const { askAI } = require('./services/openAIService');
-
-(async () => {
-  const response = await askAI("Explain photosynthesis simply and give 2 tips to remember it.");
-  console.log(response);
-})();
